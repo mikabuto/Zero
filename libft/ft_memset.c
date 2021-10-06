55cfe274c8d309c5dcd6a4478b6726b6cb51df91
urycherd <89844627+urycherd@users.noreply.github.com>
2021-10-06 10:06:21 +0300

    Update ft_memset.c

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -10,6 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include <stdio.h>
+#include <string.h>
 void	*ft_memset(void *b, int c, size_t len)
 {
 	int				i;
@@ -23,5 +26,22 @@ void	*ft_memset(void *b, int c, size_t len)
 		i++;
 		len--;
 	}
+	// k[i] = '\0';
 	return (b);
+}
+
+int	main(void)
+{
+	char len[10];
+
+	len[0] = 'a';
+	len[1] = 'b';
+	len[2] = 'c';
+	len[3] = 'd';
+	len[4] = 'e';
+	len[5] = 'f';
+	len[6] = '\0';
+	ft_memset(len, '1', 10);
+	printf("%s", len);
+	return (0);
 }
