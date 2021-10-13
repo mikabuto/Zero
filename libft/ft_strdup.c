4dfd2c84bbbe22e68ff58ca432d3c1aaf5cea494
urycherd <89844627+urycherd@users.noreply.github.com>
2021-10-13 13:02:28 +0300

    Update ft_strdup.c

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,9 +6,11 @@ char	*ft_strdup(const char *s1)
 	size_t		len;
 
 	len = ft_strlen(s1) + 1;
-	str = (char *)malloc(len * sizeof(s1));
+	str = (char *)malloc(len * sizeof(s1)); // мб тут не надо size_of, так как я присваиваю чар?
 	if (!(str))
 		return (0);
-	ft_memcpy(str, s1, len);
+	while (s1)
+		*str++ = *s1++;
+	*str = '\0';
 	return (str);
 }
