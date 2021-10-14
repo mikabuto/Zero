b1c27ad6cfe7f530f015d01b3b33a462e599bb5c
urycherd <89844627+urycherd@users.noreply.github.com>
2021-10-14 11:52:28 +0300

    Update ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -11,21 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-// #include <stdio.h>
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: urycherd <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/06 17:09:28 by urycherd          #+#    #+#             */
-/*   Updated: 2021/10/06 17:09:31 by urycherd         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
 
 size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
@@ -47,21 +32,3 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 	dst[len] = '\0';
 	return (len2 + ft_strlen(src));
 }
-
-
-// int	main(void)
-// {
-// 	char dst[9] = "01234\0";
-// 	char src[6] = "56\0789";
-// 	char dst1[9] = "01234\0";
-// 	char src1[6] = "56\0789";
-// 	int	i, f;
-
-// 	f = ft_strlcat(dst, src, 9);
-// 	printf("%s = ", dst);
-// 	i = strlcat(dst1, src1, 9);
-// 	printf("%s;\n", dst1);
-// 	printf("%d;\n", f);
-// 	printf("%d;\n", i);
-// 	return (0);
-// }
