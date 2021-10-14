36af6b4a6c8c460e4973d57e5564e5e85e5e1a40
urycherd <89844627+urycherd@users.noreply.github.com>
2021-10-14 11:52:06 +0300

    Update ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -13,23 +13,41 @@
 #include "libft.h"
 // #include <stdio.h>
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i, a;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: urycherd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 17:09:28 by urycherd          #+#    #+#             */
+/*   Updated: 2021/10/06 17:09:31 by urycherd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-	i = strlen(dst) - 1;
+#include "libft.h"
+
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+	size_t	len;
+	size_t	len2;
+	size_t	a;
+
+	len = ft_strlen(dst);
+	len2 = len;
 	a = 0;
-	if (dstsize == 0)
-		return (strlen(dst));
-	while (i < dstsize - 1)
+	if (dstsize <= len)
+		return (dstsize + ft_strlen(src));
+	while (len + 1 < dstsize)
 	{
-		dst[i + 1] = src[a];
+		dst[len] = src[a];
 		a++;
-		i++;
+		len++;
 	}
-	dst[i] = '\0';
-	return (strlen(src));
+	dst[len] = '\0';
+	return (len2 + ft_strlen(src));
 }
+
 
 // int	main(void)
 // {
